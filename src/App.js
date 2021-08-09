533431ab5d4572ca28054d2b1c8782c429d016b4
sufyankhatri <sufyankhatree@gmail.com>
2021-08-09 13:58:32 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,40 +3,66 @@ import logo from './logo.svg';
 import joypixels from 'emoji-toolkit';
 import './App.css';
 import EmojiAsset from './EmojiAsset';
+import { useState } from 'react';
 const App = () => {
   const emoji1 = (a, t) => {
     const output = `${joypixels.toImage(a)}<label>${t}$</label>`;
     console.log('output', output);
     return { __html: `${output}` };
   };
+  const [show, setshow] = useState(false);
   return (
-    <div>
-      <div className='picker'>
-        <span className='b' dangerouslySetInnerHTML={emoji1(':medal:', '10')} />
+    <div className='mainContainer'>
+      <div className='container'>
         <span
           className='b'
-          dangerouslySetInnerHTML={emoji1(':zany_face:', '10')}
+          dangerouslySetInnerHTML={emoji1(':medal:', '10')}
+          onClick={() => {
+            setshow(!show);
+          }}
         />
-        <span
-          className='b'
-          dangerouslySetInnerHTML={emoji1(':raised_hands:', '10')}
-        />
+        {show ? (
+          <div>
+            <div className='picker'>
+              <span
+                className='b'
+                dangerouslySetInnerHTML={emoji1(':medal:', '10')}
+              />
+              <span
+                className='b'
+                dangerouslySetInnerHTML={emoji1(':zany_face:', '10')}
+              />
+              <span
+                className='b'
+                dangerouslySetInnerHTML={emoji1(':raised_hands:', '10')}
+              />
 
-        <span className='b' dangerouslySetInnerHTML={emoji1(':pray:', '10')} />
-      </div>
+              <span
+                className='b'
+                dangerouslySetInnerHTML={emoji1(':pray:', '10')}
+              />
+            </div>
+            <div className='picker'>
+              <span
+                className='b'
+                dangerouslySetInnerHTML={emoji1(':medal:', '10')}
+              />
+              <span
+                className='b'
+                dangerouslySetInnerHTML={emoji1(':zany_face:', '10')}
+              />
+              <span
+                className='b'
+                dangerouslySetInnerHTML={emoji1(':raised_hands:', '10')}
+              />
 
-      <div className='picker'>
-        <span className='b' dangerouslySetInnerHTML={emoji1(':medal:', '10')} />
-        <span
-          className='b'
-          dangerouslySetInnerHTML={emoji1(':zany_face:', '10')}
-        />
-        <span
-          className='b'
-          dangerouslySetInnerHTML={emoji1(':raised_hands:', '10')}
-        />
-
-        <span className='b' dangerouslySetInnerHTML={emoji1(':pray:', '10')} />
+              <span
+                className='b'
+                dangerouslySetInnerHTML={emoji1(':pray:', '10')}
+              />
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
